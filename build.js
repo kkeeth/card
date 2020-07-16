7f9a70a64a4ef0c29c99754b575f60e76c729377
kkeeth <zensin0082@gmail.com>
2020-07-16 10:56:50 +0900

    feat: added link of Anchorfm

--- build.js
+++ build.js
@@ -20,10 +20,11 @@ const data = {
   work: chalk.white('Servant Leader of Front-end developer teams,\n              Member of the Board at Yumemi Inc.'),
   opensource: chalk.white('Riot.js Collaborator and Riot.js Japan User Group Organizer ') + chalk.green('⬢'),
   twitter: chalk.gray('https://twitter.com/') + chalk.cyan('kuwahara_jsri'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.magenta('~kkeeth'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.redBright('~kkeeth'),
   github: chalk.gray('https://github.com/') + chalk.green('kkeeth'),
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('kiyohito-keeth-kuwahara-611b09a3/'),
   standfm: chalk.gray('https://stand.fm/channels/') + chalk.cyanBright('5e70dd5881d4e84e1ff1cab4'),
+  anchorfm: chalk.gray('https://anchor.fm/') + chalk.magentaBright('kkeeth'),
   npx: chalk.red('npx') + ' ' + chalk.white('@kkeeth/card (via GitHub Package Registry @kkeeth/card)'),
   message: chalk.white.bold('              Please feel free to contact me!'),
   labelWork: chalk.white.bold('       Work:'),
@@ -33,6 +34,7 @@ const data = {
   labelGitHub: chalk.white.bold('     GitHub:'),
   labelLinkedIn: chalk.white.bold('   LinkedIn:'),
   labelStandfm: chalk.white.bold('    Standfm:'),
+  labelAnchorfm: chalk.white.bold('    Anchorfm:'),
 }
 
 // Actual strings we're going to output
@@ -45,6 +47,7 @@ const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const standfming = `${data.labelStandfm}  ${data.standfm}`
+const anchorming = `${data.labelAnchorfm}  ${data.anchorfm}`
 const messaging = data.message
 
 // Put all our output together into a single variable so we can use boxen effectively
@@ -56,7 +59,8 @@ const output = heading + // data.name + data.handle
                npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
-               standfming + newline + newline + // data.labelWeb + data.web
+               standfming + newline + // data.labelStandfm + data.standfm
+               anchorming + newline + newline + // data.labelAnchorfm + dataanchorfm
                messaging // data.message
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
