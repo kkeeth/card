511dfd8d6f327e9216152e6e7a12b4c2af83de65
Keeth Kuwahara <kiyohito.kuwahara@kaminashi.com>
2024-04-03 09:45:29 +0900

    fix: biome error

--- bin/card.js
+++ bin/card.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 // 👆 Used to tell Node.js that this is a CLI tool
 
-'use strict'
-
-const fs = require('fs')
-const path = require('path')
+const fs = require('node:fs')
+const path = require('node:path')
 const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8')
 console.log(output)
